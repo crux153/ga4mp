a4ffad0de802d4339fc8d491e0d6a7ab40703e62
Crux <crux153@gmail.com>
2023-06-17 23:21:33 +0900

    Disable nodejs require to fix webpack warning

--- src/modules/request.js
+++ src/modules/request.js
@@ -5,29 +5,29 @@ export const sendRequest = (endpoint, payload, mode = 'browser', opts = {}) => {
     if (mode === 'browser') {
         navigator?.sendBeacon([endpoint, qs].join('?'))
     } else {
-        const scheme = endpoint.split('://')[0]
-        const req = require(scheme)
-        const options = {
-            headers: {
-                'User-Agent': opts.user_agent 
-            },
-            timeout: 500,
-        }        
-        const request = req
-            .get([endpoint, qs].join('?'), options, (resp) => {
-                let data = ''
-                resp.on('data', (chunk) => {
-                    data += chunk
-                })
-                resp.on('end', () => {
-                    // TO-DO Handle Server Side Responses                    
-                })
-            })
-            .on('error', (err) => {
-                console.log('Error: ' + err.message)
-            })
-        request.on('timeout', () => {
-            request.destroy()
-        })
+        // const scheme = endpoint.split('://')[0]
+        // const req = require(scheme)
+        // const options = {
+        //     headers: {
+        //         'User-Agent': opts.user_agent 
+        //     },
+        //     timeout: 500,
+        // }        
+        // const request = req
+        //     .get([endpoint, qs].join('?'), options, (resp) => {
+        //         let data = ''
+        //         resp.on('data', (chunk) => {
+        //             data += chunk
+        //         })
+        //         resp.on('end', () => {
+        //             // TO-DO Handle Server Side Responses                    
+        //         })
+        //     })
+        //     .on('error', (err) => {
+        //         console.log('Error: ' + err.message)
+        //     })
+        // request.on('timeout', () => {
+        //     request.destroy()
+        // })
     }
 }
